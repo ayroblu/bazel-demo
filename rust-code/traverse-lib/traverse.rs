d1c6ed830b8a9c13b756866a5a7c91a060757a6a
Ben Lu <ayroblu@gmail.com>
2024-12-01 02:26:54 +0000

    wip

--- rust-code/traverse-lib/traverse.rs
+++ rust-code/traverse-lib/traverse.rs
@@ -6,34 +6,46 @@ pub enum TraverseAction {
     Next,
     Exit,
 }
-pub fn traverse_with_cursor(
+// pub fn traverse_with_cursor<'a, F, T>(
+//     cursor: &mut TreeCursor<'a>,
+//     vec: &mut Vec<T>,
+//     on_node: &F,
+// ) -> bool
+// where
+//     F: Fn(Node<'a>) -> (TraverseAction, Option<T>),
+// {
+pub fn traverse_with_cursor<T>(
     cursor: &mut TreeCursor,
-    on_node: &impl Fn(Node) -> TraverseAction,
+    vec: &mut Vec<T>,
+    on_node: &impl Fn(Node) -> (TraverseAction, Option<T>),
 ) -> bool {
     let node = cursor.node();
-    let action = if node.is_named() {
+    let (action, value) = if node.is_named() {
         on_node(node)
     } else {
-        TraverseAction::Next
+        (TraverseAction::Next, None)
     };
+    if let Some(value) = value {
+        vec.push(value);
+    }
     match action {
         TraverseAction::Continue => {
             if cursor.goto_first_child() {
-                let mut is_more = traverse_with_cursor(cursor, on_node);
+                let mut is_more = traverse_with_cursor(cursor, vec, on_node);
                 if is_more && cursor.goto_next_sibling() {
-                    is_more = traverse_with_cursor(cursor, on_node);
+                    is_more = traverse_with_cursor(cursor, vec, on_node);
                 }
                 cursor.goto_parent();
                 is_more
             } else if cursor.goto_next_sibling() {
-                traverse_with_cursor(cursor, on_node)
+                traverse_with_cursor(cursor, vec, on_node)
             } else {
                 true
             }
         }
         TraverseAction::Next => {
             if cursor.goto_next_sibling() {
-                traverse_with_cursor(cursor, on_node)
+                traverse_with_cursor(cursor, vec, on_node)
             } else {
                 true
             }
