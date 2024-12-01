8a19eed0d654bd3c0bb454de539eee9d079cfea4
Ben Lu <ayroblu@gmail.com>
2024-12-01 23:30:07 +0000

    delimiters

--- rust-code/editor-tools/lang-move/extract.rs
+++ rust-code/editor-tools/lang-move/extract.rs
@@ -1,18 +1,29 @@
 use crate::types::MoveAction;
 use tree_sitter::Node;
 
+const DELIMITERS: [&str; 3] = [",", "||", "&&"];
 pub(crate) fn get_node<'a>(
     node: &Node<'a>,
     action: &MoveAction,
 ) -> Option<Option<(Node<'a>, Node<'a>)>> {
-    match action {
-        MoveAction::Prev => node
-            .prev_named_sibling()
-            .map(|prev| Some((*node, prev)))
-            .or_else(|| node.next_named_sibling().map(|_| None)),
-        MoveAction::Next => node
-            .next_named_sibling()
-            .map(|next| Some((*node, next)))
-            .or_else(|| node.prev_named_sibling().map(|_| None)),
+    if node
+        .prev_sibling()
+        .is_some_and(|node| DELIMITERS.contains(&node.kind()))
+        || node
+            .next_sibling()
+            .is_some_and(|node| DELIMITERS.contains(&node.kind()))
+    {
+        match action {
+            MoveAction::Prev => node
+                .prev_named_sibling()
+                .map(|prev| Some((*node, prev)))
+                .or_else(|| node.next_named_sibling().map(|_| None)),
+            MoveAction::Next => node
+                .next_named_sibling()
+                .map(|next| Some((*node, next)))
+                .or_else(|| node.prev_named_sibling().map(|_| None)),
+        }
+    } else {
+        None
     }
 }
