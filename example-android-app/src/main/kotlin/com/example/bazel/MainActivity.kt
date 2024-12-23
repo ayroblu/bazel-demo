009bef2887bcf808ea727ca0f6903b861bf41ae7
Ben Lu <ayroblu@gmail.com>
2024-12-23 23:39:33 +0000

    basic theming

--- example-android-app/src/main/kotlin/com/example/bazel/MainActivity.kt
+++ example-android-app/src/main/kotlin/com/example/bazel/MainActivity.kt
@@ -5,30 +5,34 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material.Surface
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import examples.android.lib.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    Log.v("Bazel", "Hello, Android")
-    setContent { Greeting("world") }
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.v("Bazel", "Hello, Android")
+        setContent { AppTheme { MainApp() } }
+    }
 }
 
 @Preview(name = "Light Mode")
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
 @Composable
-fun Greeting(name: String) =
+fun MainApp() =
     Surface(modifier = Modifier.fillMaxSize()) {
-        Text(
-            text = "Hello $name! Why don't you work?",
-            // modifier = Modifier.background(color = Color.Black)
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Hello world!")
+        }
     }
