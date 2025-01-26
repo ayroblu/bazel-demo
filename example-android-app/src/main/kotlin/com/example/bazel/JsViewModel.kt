9de12de1f5621b14c649d369ba380adc0d09d174
Ben Lu <ayroblu@gmail.com>
2025-01-26 18:00:32 +0000

    implementation with serialization

--- example-android-app/src/main/kotlin/com/example/bazel/JsViewModel.kt
+++ example-android-app/src/main/kotlin/com/example/bazel/JsViewModel.kt
@@ -2,7 +2,6 @@ package com.example.bazel
 
 import android.app.Application
 import android.util.Log
-import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +9,8 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import com.example.bazel.genjsapp.IApp
 import com.example.bazel.genjsapp.JsDispatcher
+import com.example.bazel.genjsapp.JsReceiver
+import com.example.bazel.genjsapp.MyParams
 import com.example.bazel.jswebview.WebViewThread
 
 class JsViewModel(application: Application) : AndroidViewModel(application) {
@@ -19,8 +20,34 @@ class JsViewModel(application: Application) : AndroidViewModel(application) {
     fun handleClick() {
         name = "pre click"
         Log.v("BazelJs2", "handleClick")
-        jsAppWrapper.data.set("name", "Run once!");
-        jsAppWrapper.jsDispatcher.click()
+        jsAppWrapper.data["name"] = "Run once!"
+        jsAppWrapper.jsDispatcher.click("Run once!")
+    }
+}
+
+class JsApp(
+    private val jsViewModel: JsViewModel,
+    private val data: HashMap<String, String>
+) : IApp {
+    override fun emit(newName: String) {
+        jsViewModel.name = newName
+    }
+
+    override fun getName(): String {
+        return jsViewModel.name
+    }
+
+    override fun getData(key: String): String? {
+        return data[key]
+    }
+
+    override fun setData(key: String, value: Int) {
+        Log.v("BazelJs2", "setData: $key: $value")
+        data[key] = value.toString()
+    }
+
+    override fun emitObject(myParams: MyParams) {
+        Log.v("BazelJs2", "emitObject: $myParams")
     }
 }
 
@@ -30,11 +57,12 @@ class JsAppWrapper(private val jsViewModel: JsViewModel) {
     }
 
     val data = HashMap<String, String>()
-    val jsDispatcher = JsDispatcher(webViewThread)
+    val jsDispatcher = JsDispatcher(webViewThread, data)
 
     private fun setupWebView(webView: WebView) {
         val jsApp = JsApp(jsViewModel, data)
-        webView.addJavascriptInterface(jsApp, jsApp.appName)
+        val jsReceiver = JsReceiver(jsApp)
+        webView.addJavascriptInterface(jsReceiver, jsApp.appName)
         Log.v("BazelJs2", "Added App: ${jsApp.appName}")
         webView.evaluateJavascript("${jsApp.appName}.toString()") {
             if (it != "null") {
@@ -47,23 +75,3 @@ class JsAppWrapper(private val jsViewModel: JsViewModel) {
     }
 }
 
-class JsApp(private val jsViewModel: JsViewModel, private val data: HashMap<String, String>): IApp {
-    @JavascriptInterface
-    override fun emit(newName: String) {
-        Log.i("BazelJs2", "emit $newName")
-        jsViewModel.name = newName
-    }
-    @JavascriptInterface
-    override fun getName(): String {
-        return jsViewModel.name
-    }
-    @JavascriptInterface
-    override fun getData(key: String): String? {
-        return data[key]
-    }
-    @JavascriptInterface
-    override fun setData(key: String, value: String) {
-        Log.v("BazelJs2", value)
-        data[key] = value
-    }
-}
--- example-android-app/src/main/kotlin/com/example/bazel/genjsapp/GenJsApp.kt
+++ example-android-app/src/main/kotlin/com/example/bazel/genjsapp/GenJsApp.kt
@@ -1,35 +1,89 @@
 package com.example.bazel.genjsapp
 
+import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import com.example.bazel.jswebview.WebViewThread
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
 
 interface IApp {
     val appName: String
-        get() = "androidApp"
+        get() = "_androidApp"
 
     fun emit(newName: String)
     fun getName(): String
     fun getData(key: String): String?
-    fun setData(key: String, value: String)
+    fun setData(key: String, value: Int)
+    fun emitObject(params: MyParams)
 }
 
-class JsDispatcher(private val webViewThread: WebViewThread) {
+@Serializable
+data class MyParams(val name: String, val age: Int)
+
+val jsonParser = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    allowStructuredMapKeys = true
+}
+
+class JsReceiver(private val iJsReceiver: IApp) {
+    @JavascriptInterface
+    fun emit(newName: String) {
+        return iJsReceiver.emit(newName)
+    }
+
+    @JavascriptInterface
+    fun getName(): String {
+        return iJsReceiver.getName()
+    }
+
+    @JavascriptInterface
+    fun getData(key: String): String? {
+        return iJsReceiver.getData(key)
+    }
+
+    @JavascriptInterface
+    fun setData(key: String, value: Int) {
+        return iJsReceiver.setData(key, value)
+    }
+
+    @JavascriptInterface
+    fun emitObject(myParams: String) {
+        val data = jsonParser.decodeFromString<MyParams>(myParams)
+        return iJsReceiver.emitObject(data)
+    }
+}
+
+class JsDispatcher(private val webViewThread: WebViewThread, private val data: HashMap<String, String>) {
     fun setup(webView: WebView) {
         webView.evaluateJavascript(setupScript) {}
     }
-    private fun run(name: String) {
-        webViewThread.postCommand("$name()")
-    }
-    fun click() {
-        run("click")
+
+    fun click(name: String) {
+        data[name.hashCode().toString()] = name
+        webViewThread.postCommand("click(${name.hashCode()})")
     }
 }
 
 private const val setupScript = """
-function click() {
-    console.log("logging")
-    androidApp.setData("example", JSON.stringify(["item"]))
-    androidApp.emit(androidApp.getData("name"))
+var androidApp = {
+    emit: (name) => _androidApp.emit(name),
+    getName: () => _androidApp.getName(),
+    getData: (key) => _androidApp.getData(key),
+    setData: (key, value) => _androidApp.setData(key, value),
+    emitObject: (params) => _androidApp.emitObject(JSON.stringify(params)),
+}
+function click(code) {
+    var data = androidApp.getData(code);
+    return _click(data);
+}
+function _click(name, options) {
+    console.log("logging", name, options)
+    androidApp.setData("example", 3.14159265)
+    androidApp.emitObject({name: "John", age: 25})
+    androidApp.emit(name)
     console.log("name", androidApp.getName());
     setTimeout(() => {
         if (androidApp.getName() === "Run once!") {
