494aeeaa06683069f1a9ee6aa9d27d2e7b390e0e
Ben Lu <ayroblu@gmail.com>
2025-06-19 20:50:58 +0100

    add polling

--- g1-app/content/connect/manager.swift
+++ g1-app/content/connect/manager.swift
@@ -297,3 +297,18 @@ public class ConnectionManager {
     }
   }
 }
+
+let timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { timer in
+  log("Poll connection fired!")
+  guard let left = manager.leftPeripheral, let right = manager.rightPeripheral else {
+    log("peripheral not found")
+    return
+  }
+  for p in [left, right] {
+    if p.state != .connected {
+      manager.centralManager.connect(
+        p, options: [CBConnectPeripheralOptionNotifyOnDisconnectionKey: true])
+      log("Connecting to", p.name ?? "<unknown>")
+    }
+  }
+}
