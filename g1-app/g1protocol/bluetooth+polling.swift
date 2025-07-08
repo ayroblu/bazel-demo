d19bdd6838683f581ca01c7baa1a5d354114291a
Ben Lu <ayroblu@gmail.com>
2025-07-08 21:12:15 +0100

    poll more frequently

--- g1-app/g1protocol/bluetooth+polling.swift
+++ g1-app/g1protocol/bluetooth+polling.swift
@@ -5,19 +5,19 @@ private var timer: Timer?
 extension BluetoothManager {
   public func startTimer() {
     timer?.invalidate()
-    log("start timer")
-    timer = Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { timer in
-      log("Poll connection fired!")
+    log("BluetoothManager: start timer")
+    timer = Timer.scheduledTimer(withTimeInterval: 28.0, repeats: true) { timer in
+      log("BluetoothManager: poll connection fired!")
       guard let left = manager.leftPeripheral, let right = manager.rightPeripheral
       else {
-        log("peripheral not found")
+        log("BluetoothManager: peripheral not found")
         return
       }
       for p in [left, right] {
         if p.state != .connected {
           manager.manager.connect(
             p, options: [CBConnectPeripheralOptionNotifyOnDisconnectionKey: true])
-          log("Connecting to", p.name ?? "<unknown>")
+          log("BluetoothManager: connecting to", p.name ?? "<unknown>")
         }
       }
       transmitBoth(Device.Heartbeat.data())
