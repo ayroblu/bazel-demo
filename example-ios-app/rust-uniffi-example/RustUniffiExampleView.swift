e9c31a1a8cc1aeeb07e0a60b367ef9d8c676c621
Ben Lu <ayroblu@gmail.com>
2025-12-15 13:11:11 +0000

    demo passing closure to rust and back

--- example-ios-app/rust-uniffi-example/RustUniffiExampleView.swift
+++ example-ios-app/rust-uniffi-example/RustUniffiExampleView.swift
@@ -5,5 +5,24 @@ public struct RustUniffiExampleView: View {
   public init() {}
   public var body: some View {
     Text("add 1 + 2 = \(printAndAdd(a: 1, b: 2))")
+      .onAppear {
+        let cleanup = subber(
+          thing: ClosureWrapper {
+            print("I am a closure being called from Rust!")
+          })
+        cleanup.dispose()
+      }
+  }
+}
+
+final class ClosureWrapper: ClosureCallback {
+  let callback: @Sendable () -> Void
+
+  init(_ callback: @escaping @Sendable () -> Void) {
+    self.callback = callback
+  }
+
+  func notify() {
+    callback()
   }
 }
--- rust-code/example-shared-lib/lib.rs
+++ rust-code/example-shared-lib/lib.rs
@@ -1,7 +1,29 @@
+use std::sync::Arc;
+
 uniffi::setup_scaffolding!();
 
 #[uniffi::export]
 pub fn print_and_add(a: i32, b: i32) -> i32 {
     println!("Hello, World!");
     a + b
+}
+
+#[uniffi::export]
+pub fn subber(thing: Box<dyn ClosureCallback>) -> Arc<Cleanup> {
+    thing.notify();
+    return Arc::new(Cleanup {});
+}
+
+#[uniffi::export(callback_interface)]
+pub trait ClosureCallback: Send + Sync + 'static {
+    fn notify(&self);
+}
+
+#[derive(uniffi::Object)]
+pub struct Cleanup;
+#[uniffi::export]
+impl Cleanup {
+    fn dispose(&self) {
+        println!("dispose!");
+    }
 }
